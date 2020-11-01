6ca0be34c2cae9f769ec3158edc2404e5566fdb8
Tim Budkas <tnamhay@gmail.com>
2020-10-31 23:02:35 -0600

    added momentjs link to index.html

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
             return getWeather();
         });
         
-        
+   
         
        
     function getWeather(){
@@ -19,7 +19,7 @@ $(document).ready(function(){
                         
                         $("#showWeather").html(weatherResult);
                         $("#city").val();
-
+                        
                     }
                 });
             }
@@ -30,14 +30,24 @@ $(document).ready(function(){
         }
 
 
+        var currentDate=moment().format("MMMM Do, YYYY");
+        console.log(currentDate);   
 
 function showResults(data){
     return  '<h3> Current weather for '+data.name+' </h3>'+
+            "<p> Date:"+currentDate+" </p>"+
             "<p> Weather:"+data.weather[0].main+" </p>"+
             "<p> Temperature:"+data.main.temp+" &deg;F</p>"+
             "<p> Min temperature:"+data.main.temp_min+" </p>"+
             "<p> Max temperature:"+data.main.temp_max+" </p>"+
             "<p> Humidity:"+data.main.humidity+" </p>"+
-            "<p>Wind speed:"+data.main.speed+ " m/s</p>";
+            "<p>Wind speed:"+data.wind.speed+ " m/s</p>";
 }
 });
+
+for(var i=0; i<localStorage.length;i++)
+        {
+            var city=localStorage.getItem(i);
+            var cityName=$(".list-group").addClass("list-group-item");
+            cityName.append("<li>+city+</li>");    
+        }
